465c02c8fae41de602ce5945c12e203441b80739
0tanh <0tanhcollective@gmail.com>
2025-06-25 21:05:35 +0200

    feat(taqueria.py): implement main function for order processing and total calculation

---
+++ Lect3/taqueria/taqueria.py
@@ -0,0 +1,25 @@
+def main():
+    menu = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+    Order_Total = 0
+
+    while True:    
+        try:
+            item = input("Item: ").title()
+            Order_Total += menu[item]
+            print(f"Total: ${Order_Total:.2f}.")
+        except KeyError:
+            print("not in the list hombre.")
+        except EOFError:
+            print(f"Total: ${Order_Total:.2f}.")
+            break
+main()
