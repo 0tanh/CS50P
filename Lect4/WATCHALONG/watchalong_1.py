4c76e8a58e6fb3a8e4017e73dbef31f80cb29971
0tanh <0tanhcollective@gmail.com>
2025-06-26 15:39:20 +0200

    feat(watchalong_1.py): implement dice rolling functionality with user input

--- Lect4/WATCHALONG/watchalong_1.py
+++ Lect4/WATCHALONG/watchalong_1.py
@@ -1,3 +1,13 @@
-import random as rnd
-
-print(rnd.choice(seq))
+from random import randint 
+from random import choice
+d = int(input(("What size dize are you rolling? ")))
+print(f"You have selected a d{d}")
+while True:
+    roll_accept= input("Do you want to roll your dice? Y/N ")
+    accept_list = ["y", "Y", "YES"]
+    reject_list = ["n", "N", "NO"]
+    if roll_accept in accept_list:
+        roll = (randint(1, d))
+        print(f"You rolled a {roll}")   
+    elif roll_accept in reject_list:
+        continue
