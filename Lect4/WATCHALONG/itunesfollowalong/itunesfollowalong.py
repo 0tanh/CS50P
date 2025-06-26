2a3239cde462e1e0c0fd49f6e75d1dbfc728e445
0tanh <0tanhcollective@gmail.com>
2025-06-26 17:41:47 +0200

    feat(itunesfollowalong.py): add script to demonstrate API requests for song search

---
+++ Lect4/WATCHALONG/itunesfollowalong/itunesfollowalong.py
@@ -0,0 +1,12 @@
+# Demonstrates requests
+import json
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get(
+    "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1]
+)
+print(json.dumps(response.json(), indent=3))
