36f13b42410b7eb82ed2e9ad0dd6d87709d44597
0tanh <0tanhcollective@gmail.com>
2025-08-06 18:16:21 +0200

    fix: improve CSV reading logic and error handling in pizza.py

--- Lect6/EXCERCISES/pizza/pizza.py
+++ Lect6/EXCERCISES/pizza/pizza.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import csv
-import tabulate
+from tabulate import tabulate
 
 def main():
     if len(sys.argv) > 2: ##Error handling for bad user input
@@ -9,17 +9,20 @@ def main():
     
     Menu = sys.argv[1] #make menu variable
     
-    extension = os.path.splitext(Menu)[1] #error handling for not being a CSV
+    extension = os.path.splitext(Menu)[1] #error handling for not being a CSV using os.path.splittext method.
     if extension != ".csv":
       sys.exit("Not a CSV file")
     
     try:
         with open(Menu, "r", encoding="utf-8") as OnScreen: #opens CSV
-            reader = csv.DictReader(OnScreen) #makes variable to input into tabulation
-            print(tabulate(reader, tablefmt="grid"))
-
-    ##except if File doesn't exist.
-    except FileNotFoundError:
+            reader = csv.DictReader(OnScreen) #makes reader to read
+            All_Items =[] #makes list variable for easier tabulisation
+            for row in reader: #reads each row
+                All_Items.append(row) 
+            print(
+                tabulate(All_Items, headers= "keys", tablefmt="grid")) #makes a table from it all
+            
+    except FileNotFoundError: ##except if File doesn't exist.
         sys.exit("File Does Not Exist")
-    
+
 main()
