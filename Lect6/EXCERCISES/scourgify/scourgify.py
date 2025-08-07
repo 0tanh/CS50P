e2b1bcebac1632b01676b1adc4a8e05dafeb714d
0tanh <0tanhcollective@gmail.com>
2025-08-07 03:03:47 +0200

    refactor: improve code readability and organization in scourgify.py

--- Lect6/EXCERCISES/scourgify/scourgify.py
+++ Lect6/EXCERCISES/scourgify/scourgify.py
@@ -9,24 +9,25 @@ def main():
         sys.exit("Too Many Command Line arguments")
     
     try:
+        ##opens a reading csv as before and a writing csv as after
         with open(sys.argv[1], "r", encoding='utf-8') as Before, open(
-            sys.argv[2] ,"w", encoding= 'utf-8') as After:
+            sys.argv[2] ,"w", encoding= 'utf-8') as After: 
             
-            reader = csv.DictReader(Before)
-            new_fieldnames = ['first','last','house']
+            reader = csv.DictReader(Before) #makes a reader object for before
             
-            writer = csv.DictWriter(After, fieldnames=new_fieldnames)
-            writer.writeheader()
+            new_fieldnames = ['first','last','house'] #outlines the new fieldnames for the writer
+            writer = csv.DictWriter(After, fieldnames=new_fieldnames) #makes a writer object
+            writer.writeheader() #writes header
             
             for row in reader:
-                new_row = {}
-                Cleaned_Name = row["name"].split(", ")
-                new_row["first"] = Cleaned_Name[1] 
-                new_row["last"] = Cleaned_Name[0]
-                new_row["house"] = row.get('house', ''.strip())
-                writer.writerow(new_row)
+                new_row = {} #creates new row variable to store to
+                Cleaned_Name = row["name"].split(", ") #splits name from before
+                new_row["first"] = Cleaned_Name[1] #makes the first row
+                new_row["last"] = Cleaned_Name[0] #makes the last row
+                new_row["house"] = row.get('house', '') #makes the house row
+                writer.writerow(new_row) #writes the title row to the writer csv
             
-    except FileNotFoundError:
+    except FileNotFoundError: #error handling for before csv
         sys.exit(f"Could not read {sys.argv[1]}")
 
 if __name__ == "__main__":
